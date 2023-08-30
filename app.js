61e765d027dc865dcea0f84518a63f8768ce6288
Ariska Hidayat <ariska@qolaq.org>
2023-08-30 10:29:52 +0700

    update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 
 // biar bisa running
 app.listen(3000, () => {
-  console.log('Hai Dev, Servermu udah jalan di localhost:3000');
+  console.log('Hai Dev, Servermu udah jalan di http://localhost:3000');
 });
 // jalaninnya dengan menulis node app.js di terminal
+// matikan server dengan ctrl+c
