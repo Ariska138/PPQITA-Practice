8c26aa2d644ce0482f9aa450601f8514fc5ca93d
Ariska Hidayat <ariska@qolaq.org>
2023-09-06 12:51:48 +0700

    refactoring post data

---
+++ handlers/post-data-user-handler.js
@@ -0,0 +1,38 @@
+const { nameValidation, ageValidation } = require('../validation/validation');
+
+const postDataUserHandler = (req, res) => {
+  // mengecek property name
+  if (!req.body.name) {
+    res.status(400);
+    return res.send({ error: true, message: 'tidak memiliki paramater nama' });
+  }
+  // @todo pengecekan property umur
+  if (!req.body.age) {
+    res.status(400);
+    return res.send({ error: true, message: 'tidak memiliki paramater umur' });
+  }
+
+  // mengambil data nama
+  // let name = req.body.name; // mengambil data name
+  let { name, age } = req.body;
+  // @todo pemanggilan data umur
+
+  // melakukan validasi nama
+  let realNameRes = nameValidation(name);
+  let realAgeRes = ageValidation(age);
+  // @todo melakukan validasi umur
+
+  if (realNameRes.error) {
+    res.status(400);
+    return res.send(realNameRes);
+  }
+  // @todo lempar error umur
+  if (realAgeRes.error) {
+    res.status(400);
+    return res.send(realAgeRes);
+  }
+
+  res.send({ data: { name: realNameRes.data, age: realAgeRes.data } }); // @todo menambahkan data umur yg sudah divalidasi
+};
+
+module.exports = { postDataUserHandler };
